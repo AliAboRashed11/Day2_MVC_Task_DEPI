5e15f3badc7a07dc556aab74e45d7bba1a6a9050
AliAboRashed11 <aborashedali14@gmail.com>
2024-09-22 19:54:33 +0300

    Add project files.

---
+++ Controllers/GetDataController.cs
@@ -0,0 +1,74 @@
+﻿using Day2.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace Day2.Controllers
+{
+    public class GetDataController : Controller
+    {
+        DBContextCourse course = new DBContextCourse();
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult GetallInstructor()
+        {
+
+            var Instructors = course.Instructors.Include(a => a.Course).Include(y => y.Department).ToList();
+            return View("GetallInstructor", Instructors);
+        }
+
+        public IActionResult GetallTran()
+        {
+            ViewData["Name"] = "Ali";
+            var Trainees = course.Trainees.Include(y => y.Department).ToList();
+            return View("GetallTran", Trainees);
+        }
+
+
+
+
+        public IActionResult GetallInstId(int id)
+        {
+            ViewData["Name"] = "Instructor";
+            var Instractor = course.Instructors.Include(a => a.Course).Include(y => y.Department).FirstOrDefault(a => a.Id == id);
+            return View("GetallInstId", Instractor);
+        }
+
+
+        public IActionResult deleteId(int id)
+        {
+
+
+            var Instractor = course.Instructors.FirstOrDefault(a => a.Id==id);
+            course.Instructors.Remove(Instractor);
+            course.SaveChanges();
+
+            return Content(" Instructor has deleted ");
+        }
+
+        public IActionResult AddInstructor(Instructor instructor)
+        {
+
+
+            course.Instructors.Add(instructor);
+
+            course.SaveChanges();
+
+
+            return Content(" Hellow worled ");
+        }
+        public IActionResult UpdateInstructor(int id)
+        {
+
+            var instructor = course.Instructors.Include(x => x.Course).Include(x => x.Department).FirstOrDefault(x => x.Id == id);
+
+            course.SaveChanges();
+
+
+            return Content(" Instructor has updated ");
+        }
+
+    }
+}
